0f8dee8b315e509457d6b02a6a1f45e937ac21e5
Wesley Scholl <clients@konjoweb.com>
2019-06-22 10:18:52 -0400

    added mybuzz hello world

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json("MyBuzz");
+});
+
 app.set("port", process.env.PORT || 5000);
 
 app.listen(app.get("port"), () => {
