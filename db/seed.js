325c0cfd72db3581c40d0e791686c5192d537ce2
Wesley Scholl <clients@konjoweb.com>
2019-06-22 11:06:17 -0400

    added seed file and connected database

---
+++ db/seed.js
@@ -0,0 +1,38 @@
+const mongoose = require("./connection");
+const User = require("../models/user");
+
+mongoose.Promise = Promise;
+
+User.deleteMany({})
+  .then(() => {
+    User.create({
+      name: "Charles",
+      gender: "Male",
+      weight: 180,
+      email: "c.blundon@gmail.com",
+      password: "Test123",
+      buzzes: [
+        {
+          numberOfDrinks: 2,
+          drinkType: "Beer",
+          hours: 1
+        }
+      ]
+    }).then(console.log("User Created"));
+  })
+  .then(console.log("Users Deleted"));
+
+User.create({
+  name: "Wesley",
+  gender: "Male",
+  weight: 210,
+  email: "wesleyscholl@gmail.com",
+  password: "konjo1234",
+  buzzes: [
+    {
+      numberOfDrinks: 4,
+      drinkType: "Beer",
+      hours: 2
+    }
+  ]
+}).then(console.log("User Created"));
