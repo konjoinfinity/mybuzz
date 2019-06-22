c08e849ab549f38f67ad0509456c9d9bf4ac9347
Wesley Scholl <clients@konjoweb.com>
2019-06-22 10:12:58 -0400

    activated express, set app to listen on port

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 5000);
+
+app.listen(app.get("port"), () => {
+  console.log(`Running on PORT: ${app.get("port")}`);
+});
