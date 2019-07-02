b223f81a64921e342fea2f82a4d329bf41cd4708
Wesley Scholl <clients@konjoweb.com>
2019-07-02 17:22:42 -0400

    updated seed data to play nicely with passport auth

--- db/seed.js
+++ db/seed.js
@@ -47,184 +47,239 @@ User.deleteMany({})
   })
   .then(console.log("Users Deleted"));
 
-// User.register({
-//   username: "wesleyscholl@gmail.com",
-//   password: "777bmx777",
-//   name: "Wesley",
-//   gender: "Male",
-//   weight: 210,
-//   oldbuzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Beer",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Beer",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     }
-//   ]
-// }).then(console.log("User Created"));
+User.register(
+  {
+    username: "wesleyscholl@gmail.com",
+    name: "Wesley",
+    gender: "Male",
+    weight: 210,
+    oldbuzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Beer",
+        hours: 1,
+        dateCreated: Date.now()
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Beer",
+        hours: 1,
+        dateCreated: Date.now()
+      }
+    ]
+  },
+  "777bmx777",
+  (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("User Created");
+    }
+  }
+);
 
-// User.register({
-//   name: "Julie",
-//   gender: "Female",
-//   weight: 150,
-//   username: "julie@julie.com",
-//   password: "whywhywhy",
-//   buzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Wine",
-//       hours: 1
-//     }
-//   ],
-//   oldbuzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Wine",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Wine",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     }
-//   ]
-// }).then(console.log("User Created"));
-//
-// User.register({
-//   name: "Brian",
-//   gender: "Male",
-//   weight: 190,
-//   username: "brian@brian.com",
-//   password: "hellohello",
-//   buzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Liquor",
-//       hours: 1
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Liquor",
-//       hours: 1
-//     }
-//   ],
-//   oldbuzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Liquor",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Liquor",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     }
-//   ]
-// }).then(console.log("User Created"));
-//
-// User.register({
-//   name: "Tim",
-//   gender: "Male",
-//   weight: 210,
-//   username: "tim@tim.com",
-//   password: "byebyebye",
-//   buzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Liquor",
-//       hours: 1
-//     }
-//   ],
-//   oldbuzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Beer",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Wine",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     }
-//   ]
-// }).then(console.log("User Created"));
-//
-// User.register({
-//   name: "Kate",
-//   gender: "Female",
-//   weight: 140,
-//   username: "kate@kate.com",
-//   password: "sighsighsigh",
-//   buzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Beer",
-//       hours: 1
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Beer",
-//       hours: 1
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Beer",
-//       hours: 1
-//     }
-//   ],
-//   oldbuzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Beer",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Beer",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     }
-//   ]
-// }).then(console.log("User Created"));
-//
-// User.register({
-//   name: "James",
-//   gender: "Male",
-//   weight: 200,
-//   username: "james@james.com",
-//   password: "hihihi",
-//   buzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Wine",
-//       hours: 1
-//     }
-//   ],
-//   oldbuzzes: [
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Wine",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     },
-//     {
-//       numberOfDrinks: 1,
-//       drinkType: "Wine",
-//       hours: 1,
-//       dateCreated: Date.now()
-//     }
-//   ]
-// }).then(console.log("User Created"));
+User.register(
+  {
+    username: "julie@julie.com",
+    name: "Julie",
+    gender: "Female",
+    weight: 150,
+    buzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Wine",
+        hours: 1
+      }
+    ],
+    oldbuzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Wine",
+        hours: 1,
+        dateCreated: Date.now()
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Wine",
+        hours: 1,
+        dateCreated: Date.now()
+      }
+    ]
+  },
+  "whywhywhy",
+  (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("User Created");
+    }
+  }
+);
+
+User.register(
+  {
+    username: "brian@brian.com",
+    name: "Brian",
+    gender: "Male",
+    weight: 190,
+    buzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Liquor",
+        hours: 1
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Liquor",
+        hours: 1
+      }
+    ],
+    oldbuzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Liquor",
+        hours: 1,
+        dateCreated: Date.now()
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Liquor",
+        hours: 1,
+        dateCreated: Date.now()
+      }
+    ]
+  },
+  "hellohello",
+  (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("User Created");
+    }
+  }
+);
+
+User.register(
+  {
+    username: "tim@tim.com",
+    name: "Tim",
+    gender: "Male",
+    weight: 210,
+    buzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Liquor",
+        hours: 1
+      }
+    ],
+    oldbuzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Beer",
+        hours: 1,
+        dateCreated: Date.now()
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Wine",
+        hours: 1,
+        dateCreated: Date.now()
+      }
+    ]
+  },
+  "byebyebye",
+  (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("User Created");
+    }
+  }
+);
+
+User.register(
+  {
+    username: "kate@kate.com",
+    name: "Kate",
+    gender: "Female",
+    weight: 140,
+
+    buzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Beer",
+        hours: 1
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Beer",
+        hours: 1
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Beer",
+        hours: 1
+      }
+    ],
+    oldbuzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Beer",
+        hours: 1,
+        dateCreated: Date.now()
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Beer",
+        hours: 1,
+        dateCreated: Date.now()
+      }
+    ]
+  },
+  "sighsighsigh",
+  (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("User Created");
+    }
+  }
+);
+
+User.register(
+  {
+    username: "james@james.com",
+    name: "James",
+    gender: "Male",
+    weight: 200,
+    buzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Wine",
+        hours: 1
+      }
+    ],
+    oldbuzzes: [
+      {
+        numberOfDrinks: 1,
+        drinkType: "Wine",
+        hours: 1,
+        dateCreated: Date.now()
+      },
+      {
+        numberOfDrinks: 1,
+        drinkType: "Wine",
+        hours: 1,
+        dateCreated: Date.now()
+      }
+    ]
+  },
+  "hihihi",
+  (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("User Created");
+    }
+  }
+);
