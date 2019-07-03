b2a41c5ce40f6a0d2063dff9050e16bef235ae26
Wesley Scholl <clients@konjoweb.com>
2019-07-03 10:58:34 -0400

    removed console logs from auth user function

--- auth/authuser.js
+++ auth/authuser.js
@@ -1,9 +1,7 @@
 module.exports = function authenticatedUser(req, res, next) {
   if (req.isAuthenticated()) {
-    console.log("authenticated");
     return next();
   } else {
-    console.log("not authenticated");
     req.flash("info", "Please Login or Signup");
     res.redirect("/login");
   }
